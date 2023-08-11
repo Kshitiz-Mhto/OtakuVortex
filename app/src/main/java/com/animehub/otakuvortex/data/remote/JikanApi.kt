d85f52e437a0b0acb6f4e8b83d0e3188e1d8ed4a
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-12 05:37:05 +0545

    corrected the return class

--- app/src/main/java/com/animehub/otakuvortex/data/remote/JikanApi.kt
+++ app/src/main/java/com/animehub/otakuvortex/data/remote/JikanApi.kt
@@ -1,8 +1,8 @@
 package com.animehub.otakuvortex.data.remote
 
 import com.animehub.otakuvortex.data.remote.dto.anime.topanime.TopAnimeDto
+import com.animehub.otakuvortex.data.remote.dto.manga.topmanga.TopMangaDto
 import com.animehub.otakuvortex.data.remote.dto.topcharacter.TopCharacterDto
-import com.animehub.otakuvortex.domain.modal.mamga.topmanga.TopMangaData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,7 +12,7 @@ interface JikanApi {
     suspend fun getTopAnime(@Query("page") page: Int): TopAnimeDto
 
     @GET("/v4/top/manga")
-    suspend fun getTopManga(@Query("page") page: Int): TopMangaData
+    suspend fun getTopManga(@Query("page") page: Int): TopMangaDto
 
     @GET("/v4/top/characters")
     suspend fun getTopCharacters(@Query("page") page: Int): TopCharacterDto
