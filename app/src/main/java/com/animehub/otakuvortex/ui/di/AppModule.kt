722059e9f2f271df907d3fa08805b7624c2ea222
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-12 04:49:25 +0545

    added app module for dependency injection for Jikan Api

--- app/src/main/java/com/animehub/otakuvortex/ui/di/AppModule.kt
+++ app/src/main/java/com/animehub/otakuvortex/ui/di/AppModule.kt
@@ -1,4 +1,41 @@
 package com.animehub.otakuvortex.ui.di
 
+import com.animehub.otakuvortex.data.remote.JikanApi
+import com.animehub.otakuvortex.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideJikanApi(): JikanApi{
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(25, TimeUnit.SECONDS)
+        }.build()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(JikanApi::class.java)
+    }
+
 }
