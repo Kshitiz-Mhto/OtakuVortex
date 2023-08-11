bf96600ba9488988195c8f0fc7c399bb19f76632
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-11 18:39:38 +0545

    added auto navigation code after 3 sec

--- app/src/main/java/com/animehub/otakuvortex/ui/LandingFragment.kt
+++ app/src/main/java/com/animehub/otakuvortex/ui/LandingFragment.kt
@@ -1,15 +1,21 @@
 package com.animehub.otakuvortex.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.animehub.otakuvortex.R
 import com.animehub.otakuvortex.databinding.FragmentLandingBinding
 
 class LandingFragment : Fragment() {
 
     private lateinit var binding: FragmentLandingBinding
+    private var delayedNavigation: Runnable? = null
+    private val handler: Handler by lazy { Handler() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,39 @@ class LandingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLandingBinding.inflate(inflater, container, false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // For API 28 and above, use postDelayed directly on the View
+            delayedNavigation = Runnable {
+                navigateToHomeFragment()
+            }
+            binding.root.postDelayed(delayedNavigation!!, 3000)
+        } else {
+            // For API versions less than 28, use the Handler
+            delayedNavigation = Runnable {
+                navigateToHomeFragment()
+            }
+            handler.postDelayed(delayedNavigation!!, 3000)
+        }
+
         return binding.root
+    }
+
+    private fun navigateToHomeFragment() {
+        findNavController().navigate(
+            R.id.action_landingFragment_to_homeFragment
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        delayedNavigation?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                binding.root.removeCallbacks(it)
+            } else {
+                handler.removeCallbacks(it)
+            }
+        }
     }
 
 }
