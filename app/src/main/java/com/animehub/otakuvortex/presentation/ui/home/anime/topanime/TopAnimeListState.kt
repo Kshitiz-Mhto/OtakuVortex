8de99f121465234cbe367c15ee66759a202fbab2
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-20 16:55:31 +0545

    feat: added response state for fetched anime list

--- app/src/main/java/com/animehub/otakuvortex/presentation/ui/home/anime/topanime/TopAnimeListState.kt
+++ app/src/main/java/com/animehub/otakuvortex/presentation/ui/home/anime/topanime/TopAnimeListState.kt
@@ -1,9 +1,10 @@
 package com.animehub.otakuvortex.presentation.ui.home.anime.topanime
 
+import androidx.paging.PagingData
 import com.animehub.otakuvortex.domain.modal.anime.topanime.TopAnimeData
 
 data class TopAnimeListState(
     val isLoading: Boolean = false,
-    val topAnimeList: List<TopAnimeData> = emptyList(),
-    val error: String = ""
+    val error: String = "",
+    val topAnimeList: PagingData<TopAnimeData>? = null
 )
