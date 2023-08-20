00646e2b985fae8ec264b37a9b075f00bb4b1827
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-20 16:53:01 +0545

    FEAT: added usecase layer for the handling the response form api

--- app/src/main/java/com/animehub/otakuvortex/domain/use_case/anime/TopAnimeUseCase.kt
+++ app/src/main/java/com/animehub/otakuvortex/domain/use_case/anime/TopAnimeUseCase.kt
@@ -1,27 +1,25 @@
 package com.animehub.otakuvortex.domain.use_case.anime
 
-import com.animehub.otakuvortex.data.remote.dto.anime.topanime.toTopAnimeData
+import androidx.paging.PagingData
 import com.animehub.otakuvortex.domain.modal.anime.topanime.TopAnimeData
 import com.animehub.otakuvortex.domain.repository.anime.AnimeRepository
 import com.animehub.otakuvortex.util.ResponseState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
-import retrofit2.Response
 import java.io.IOException
 import javax.inject.Inject
 
 class TopAnimeUseCase @Inject constructor(
     private val repo: AnimeRepository
 ) {
-
-    operator fun invoke(page: Int): Flow<ResponseState<List<TopAnimeData>>> = flow{
+    operator fun invoke(): Flow<ResponseState<PagingData<TopAnimeData>>> = flow{
         try {
             emit(ResponseState.Loading())
-            val topAnimes = repo.getTopAnime(page).data.map {
-                it.toTopAnimeData()
+            repo.getTopAnime().collect(){
+//                Log.i("info",it.toString())
+                emit(ResponseState.Success(it))
             }
-            emit(ResponseState.Success(topAnimes))
         }catch (e: HttpException){
             emit(ResponseState.Error(e.localizedMessage ?: "Unexcepted Error occured!!"))
         }catch (e: IOException){
