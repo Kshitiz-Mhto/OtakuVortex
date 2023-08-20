47159cdc81fb17388535c9f97d4c5dc397482c48
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-20 16:58:00 +0545

    BREAKING CHANGE: added viewmodel for handling the flow from anime usecase

--- app/src/main/java/com/animehub/otakuvortex/presentation/ui/home/anime/topanime/TopAnimeViewModel.kt
+++ app/src/main/java/com/animehub/otakuvortex/presentation/ui/home/anime/topanime/TopAnimeViewModel.kt
@@ -1,5 +1,6 @@
 package com.animehub.otakuvortex.presentation.ui.home.anime.topanime
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.animehub.otakuvortex.domain.use_case.anime.TopAnimeUseCase
@@ -16,22 +17,42 @@ class TopAnimeViewModel @Inject constructor(
     private val topAnimeUseCase: TopAnimeUseCase
 ): ViewModel() {
 
-    private val topAnimeListValue = MutableStateFlow(TopAnimeListState())
-    var _topAnimeListValue: StateFlow<TopAnimeListState> = topAnimeListValue
+    init {
+        getTopAnimeList()
+    }
 
-    fun getTopAnimeList(page:Int) = viewModelScope.launch(Dispatchers.IO){
-        topAnimeUseCase(page = page).collect{
-            when(it){
-                is ResponseState.Success -> {
-                    topAnimeListValue.value = TopAnimeListState(topAnimeList = it.data?: emptyList())
-                }
-                is ResponseState.Loading -> {
-                    topAnimeListValue.value = TopAnimeListState(isLoading = true)
-                }
-                is ResponseState.Error -> {
-                    topAnimeListValue.value = TopAnimeListState(error = it.message ?: "An Unexcepted Error")
+    private val _topAnimeListValue = MutableStateFlow(TopAnimeListState())
+    var topAnimeListValue: StateFlow<TopAnimeListState> = _topAnimeListValue
+
+
+    fun getTopAnimeList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            topAnimeUseCase().collect { responseState ->
+                when (responseState) {
+                    is ResponseState.Success -> {
+                        val topAnimePagingData = TopAnimeListState(topAnimeList = responseState.data)
+                        _topAnimeListValue.value = topAnimePagingData
+                        Log.i("value", responseState.data.toString())
+                        Log.i("value1", _topAnimeListValue.value.toString())
+//                        Log.i("data", responseState.data.toString())
+//                        Log.i("val", _topAnimeListValue.value!!.topAnimeList.toString())
+                        }
+                    is ResponseState.Loading -> {
+                        val topAnimeListState = TopAnimeListState(isLoading = true)
+                        _topAnimeListValue.value = (topAnimeListState)
+//                        Log.i("load", _topAnimeListValue.value.toString())
+                    }
+                    is ResponseState.Error -> {
+                        val topAnimeListState = TopAnimeListState(error = responseState.message ?: "An Unexpected Error")
+                        _topAnimeListValue.value = topAnimeListState
+//                        Log.i("error", _topAnimeListValue.value.toString())
+                    }
+                    else -> {
+                        // Handle other cases if needed
+                    }
                 }
             }
         }
     }
+
 }
