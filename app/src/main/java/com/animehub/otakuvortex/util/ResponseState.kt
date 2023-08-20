d2e1ab2b9fb29c6a8377a501f2ca898718b0496d
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-20 17:40:53 +0545

    refactor: removed uncessary responses

--- app/src/main/java/com/animehub/otakuvortex/util/ResponseState.kt
+++ app/src/main/java/com/animehub/otakuvortex/util/ResponseState.kt
@@ -1,14 +1,8 @@
 package com.animehub.otakuvortex.util
 
-import androidx.paging.PagingData
-import com.animehub.otakuvortex.domain.modal.mamga.topmanga.TopMangaData
-import com.animehub.otakuvortex.domain.modal.topcharacter.TopCharacterModel
-import kotlinx.coroutines.flow.Flow
 
 sealed class ResponseState<T>(val data :T?=null,val message:String?=null){
     class Loading<T>(data:T?=null):ResponseState<T>(data)
     class Success<T>(data: T):ResponseState<T>(data)
-    class Success1<T>(data: Flow<PagingData<TopCharacterModel>>):ResponseState<T>()
-    class Success2<T>(data: Flow<PagingData<TopMangaData>>):ResponseState<T>()
     class Error<T>(message:String,data:T?=null):ResponseState<T>(data,message)
 }
