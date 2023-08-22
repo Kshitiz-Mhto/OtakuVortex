1441304aa3727f351c8416fdcd4de76c949e9e65
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-22 12:08:08 +0545

    feat: instead of landing frag, we added foreground as a splash

--- app/src/main/java/com/animehub/otakuvortex/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/animehub/otakuvortex/presentation/ui/MainActivity.kt
@@ -2,13 +2,28 @@ package com.animehub.otakuvortex.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.animehub.otakuvortex.R
+import com.animehub.otakuvortex.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        Handler().postDelayed({
+            binding.initialActivity.foreground = null
+        }, 3000)
+
+        binding.bottomNavigationView.setupWithNavController(findNavController(R.id.fragmentContainerView))
     }
 }
