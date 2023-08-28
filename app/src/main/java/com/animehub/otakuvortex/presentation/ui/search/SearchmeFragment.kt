55866f99eb3b705670652d47d8621c02af8dbdae
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-28 21:01:41 +0545

    BREAKING CHANGE: added new fragment to handle searched anime and manga, further UI components are added in xml layout

---
+++ app/src/main/java/com/animehub/otakuvortex/presentation/ui/search/SearchmeFragment.kt
@@ -0,0 +1,126 @@
+package com.animehub.otakuvortex.presentation.ui.search
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.animehub.otakuvortex.databinding.FragmentSearchmeBinding
+import com.animehub.otakuvortex.paging.anime.searched.SearchedAnimePagingAdaptor
+import com.animehub.otakuvortex.paging.loaderadapter.LoaderAdaptor
+import com.animehub.otakuvortex.paging.manga.searched.SearchedMangaPagingAdaptor
+import com.animehub.otakuvortex.presentation.ui.search.searchanime.SearchedAnimeViewModel
+import com.animehub.otakuvortex.presentation.ui.search.searchmanga.SearchedMangaViewModel
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.util.Locale
+
+@AndroidEntryPoint
+class SearchmeFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchmeBinding
+
+    private lateinit var searchedAnimeRecyclerview: RecyclerView
+    private lateinit var searchedMangaRecyclerView: RecyclerView
+    private val searchedAnimeViewModel: SearchedAnimeViewModel by viewModels()
+    private val searchedMangaViewModel: SearchedMangaViewModel by viewModels()
+    private lateinit var searchedAnimePagingAdaptor: SearchedAnimePagingAdaptor
+    private lateinit var searchedMangaPagingAdaptor: SearchedMangaPagingAdaptor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchmeBinding.inflate(inflater, container, false)
+
+        searchedAnimeRecyclerview = binding.searchedAnimeRecylervew
+        searchedMangaRecyclerView = binding.searchedMangaRecylervew
+        searchedMangaRecyclerView.setBackgroundColor(Color.TRANSPARENT)
+        searchedAnimeRecyclerview.setBackgroundColor(Color.TRANSPARENT)
+        searchedMangaRecyclerView.layoutManager = GridLayoutManager(requireContext(),2, GridLayoutManager.HORIZONTAL, false)
+        searchedAnimeRecyclerview.layoutManager = GridLayoutManager(requireContext(),2, GridLayoutManager.HORIZONTAL, false)
+        searchedAnimePagingAdaptor = SearchedAnimePagingAdaptor()
+        searchedMangaPagingAdaptor = SearchedMangaPagingAdaptor()
+
+        binding.searchResult.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val searchText = query!!.lowercase(Locale.getDefault())
+                if (searchText.isNotEmpty() or searchText.isNotBlank()){
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        searchedAnimeViewModel.searchedAnimeListValue.collect{ state ->
+                            when{
+                                state.isLoading -> {
+                                    Log.i("UI load", "lol")
+                                }
+                                state.error.isNotEmpty() -> {
+                                    Log.i("UI error", "lol")
+                                }
+                                else -> {
+                                    val animeList = state.searchedAnimeList
+                                    if(animeList != null) {
+                                        searchedAnimePagingAdaptor.submitData(
+                                            lifecycle,
+                                            animeList
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        searchedMangaViewModel.searchedMangaListValue.collect{ stateo ->
+                            when{
+                                stateo.isLoading -> {
+                                    Log.i("UI load", "lol")
+                                }
+                                stateo.error.isNotEmpty() -> {
+                                    Log.i("UI error", "lol")
+                                }
+                                else -> {
+                                    val mangaList = stateo.searchedMangaList
+                                    if(mangaList != null) {
+                                        searchedMangaPagingAdaptor.submitData(
+                                            lifecycle,
+                                            mangaList
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        searchedAnimeRecyclerview.adapter = searchedAnimePagingAdaptor.withLoadStateHeaderAndFooter(
+            header = LoaderAdaptor(),
+            footer = LoaderAdaptor()
+        )
+
+        searchedMangaRecyclerView.adapter = searchedMangaPagingAdaptor.withLoadStateHeaderAndFooter(
+            header = LoaderAdaptor(),
+            footer = LoaderAdaptor()
+        )
+
+        return binding.root
+    }
+
+}
