461f2f4d8131fe575239b50c5c852f6312786a45
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-28 20:53:17 +0545

    feat: added custom model needed for our UI

---
+++ app/src/main/java/com/animehub/otakuvortex/domain/modal/anime/searchanime/SearchedAnimeData.kt
@@ -0,0 +1,7 @@
+package com.animehub.otakuvortex.domain.modal.anime.searchanime
+
+data class SearchedAnimeData (
+    val animeId: Int,
+    val imageUrl: String,
+    val title: String
+    )
---
+++ app/src/main/java/com/animehub/otakuvortex/domain/modal/mamga/searchmanga/SearchedMangaData.kt
@@ -0,0 +1,7 @@
+package com.animehub.otakuvortex.domain.modal.mamga.searchmanga
+
+data class SearchedMangaData(
+    val animeId: Int,
+    val imageUrl: String,
+    val title: String
+    )
