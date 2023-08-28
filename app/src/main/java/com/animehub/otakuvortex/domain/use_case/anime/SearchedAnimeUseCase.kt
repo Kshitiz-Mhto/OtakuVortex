0c3321bf0b3491d3c3c0974943aeeebee3407348
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-28 20:54:00 +0545

    BREAKING CHANGE: added use cases for searched manga and anime query

---
+++ app/src/main/java/com/animehub/otakuvortex/domain/use_case/anime/SearchedAnimeUseCase.kt
@@ -0,0 +1,32 @@
+package com.animehub.otakuvortex.domain.use_case.anime
+
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.animehub.otakuvortex.domain.modal.anime.searchanime.SearchedAnimeData
+import com.animehub.otakuvortex.domain.repository.anime.SearchedAnimeRepository
+import com.animehub.otakuvortex.util.ResponseState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import kotlin.coroutines.coroutineContext
+
+class SearchedAnimeUseCase @Inject constructor(
+    private val repo: SearchedAnimeRepository,
+) {
+
+    operator fun invoke(query: String): Flow<ResponseState<PagingData<SearchedAnimeData>>> = flow {
+        try {
+            emit(ResponseState.Loading())
+            repo.getSearchedAnime(query).cachedIn(CoroutineScope(coroutineContext)).collect(){
+                emit(ResponseState.Success(it))
+            }
+        }catch (e: HttpException){
+            emit(ResponseState.Error(e.localizedMessage ?: "Unexcepted Error occured!!"))
+        }catch (e: IOException){
+            emit(ResponseState.Error(e.localizedMessage ?: "Internet Connection Error!!"))
+        }
+    }
+}
---
+++ app/src/main/java/com/animehub/otakuvortex/domain/use_case/manga/SearchedMangaUseCase.kt
@@ -0,0 +1,32 @@
+package com.animehub.otakuvortex.domain.use_case.manga
+
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.animehub.otakuvortex.domain.modal.mamga.searchmanga.SearchedMangaData
+import com.animehub.otakuvortex.domain.repository.manga.SearchedMangaRepository
+import com.animehub.otakuvortex.util.ResponseState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import kotlin.coroutines.coroutineContext
+
+class SearchedMangaUseCase @Inject constructor(
+    private val repo: SearchedMangaRepository
+) {
+
+    operator fun invoke(query: String): Flow<ResponseState<PagingData<SearchedMangaData>>> = flow{
+        try {
+            emit(ResponseState.Loading())
+            repo.getSearchedManga(query).cachedIn(CoroutineScope(coroutineContext)).collect(){
+                emit(ResponseState.Success(it))
+            }
+        }catch (e: HttpException){
+            emit(ResponseState.Error(e.localizedMessage ?: "Unexcepted Error occured!!"))
+        }catch (e: IOException){
+            emit(ResponseState.Error(e.localizedMessage ?: "Internet Connection Error!!"))
+        }
+    }
+}
