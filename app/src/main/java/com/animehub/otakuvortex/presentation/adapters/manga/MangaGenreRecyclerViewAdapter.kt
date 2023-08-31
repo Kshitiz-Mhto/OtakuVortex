30cfab164ae39d715d31feb17ac5c40b9f207074
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-31 23:27:30 +0545

    feat: added adapter for character and manga infovie

--- app/src/main/java/com/animehub/otakuvortex/presentation/adapters/manga/MangaGenreRecyclerViewAdapter.kt
+++ app/src/main/java/com/animehub/otakuvortex/presentation/adapters/manga/MangaGenreRecyclerViewAdapter.kt
@@ -1,4 +1,4 @@
-package com.animehub.otakuvortex.presentation.adapters
+package com.animehub.otakuvortex.presentation.adapters.manga
 
 import android.view.LayoutInflater
 import android.view.View
@@ -7,19 +7,19 @@ import android.widget.Button
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.animehub.otakuvortex.R
-import com.animehub.otakuvortex.data.remote.dto.anime.animebyid.Genre
+import com.animehub.otakuvortex.data.remote.dto.manga.mangabyid.Genre
 
-class AnimeGenreRecyclerViewAdapter: ListAdapter<Genre, AnimeGenreRecyclerViewAdapter.GenreViewHolder>(DiffUtily()) {
+class MangaGenreRecyclerViewAdapter: ListAdapter<Genre, MangaGenreRecyclerViewAdapter.GenreViewHolder>(DiffUtily()) {
 
-    inner class GenreViewHolder(view: View): RecyclerView.ViewHolder(view){
+    inner class GenreViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val genreTitle = view.findViewById<Button>(R.id.btnInfoGenre)
 
-        fun bind(item: Genre){
+        fun bind(item: Genre) {
             genreTitle.text = item.name
         }
     }
 
-    class DiffUtily : androidx.recyclerview.widget.DiffUtil.ItemCallback<Genre>(){
+    class DiffUtily : androidx.recyclerview.widget.DiffUtil.ItemCallback<Genre>() {
         override fun areItemsTheSame(oldItem: Genre, newItem: Genre): Boolean {
             return oldItem.mal_id == newItem.mal_id
         }
@@ -31,19 +31,19 @@ class AnimeGenreRecyclerViewAdapter: ListAdapter<Genre, AnimeGenreRecyclerViewAd
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenreViewHolder {
         return GenreViewHolder(
-                LayoutInflater.from(parent.context)
-                    .inflate(
-                        R.layout.genre_layout,
-                        parent,
-                        false
-                    )
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    R.layout.genre_layout,
+                    parent,
+                    false
                 )
+        )
     }
 
     override fun onBindViewHolder(holder: GenreViewHolder, position: Int) {
         val indexElement = getItem(position)
         holder.bind(indexElement)
     }
-
-
 }
+
+
